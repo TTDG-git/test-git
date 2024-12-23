d2d71b41b22f0c6c016263a52c3cc2d2abc384b5
TTDG-git <dgtsakala@gmail.com>
2024-12-23 12:11:20 +0100

    Ajout fichier javascrit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("hello world");
+// Affiche un message de bienvenue dans la console
+function afficherMessageBienvenue() {
+    console.log("Bienvenue dans le monde de JavaScript !");
+}
+
+// Appelle la fonction lorsque la page est complètement chargée
+window.onload = afficherMessageBienvenue;
